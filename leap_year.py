8fdb92b568b5e140d1e98bfb3c0311cb60fc1675
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-14 18:20:57 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,8 @@
 def leap_year():
+    # Solicitar el año al usuario
     anio = int(input("Ingrese un año: "))
+
+    # Verificar si es bisiesto según las reglas del calendario gregoriano
     if (anio % 4 == 0) and (anio % 100 != 0 or anio % 400 == 0):
         print(f"El año {anio} es bisiesto")
     else:
